2007fbaa9b4ede2ff1dc915cf4bfb591279ef46b
Dylan Sund <dylansund21@gmail.com>
2021-05-03 10:44:26 -0700

    update

---
+++ training/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='Training-Home'),
+    path('about/', views.about, name='Training-About'),
+    
+    path('visualize/', views.visualize, name='Training-Visualize'),
+    path('visualize/throwing/', views.visualizeThrow, name='Training-visualizeThrow'),
+    path('visualize/lifting/', views.visualizeLift, name='Training-visualizeLift'),
+
+    path('athlete-dashboard/<int:year>/<int:month>/<int:day>', views.scheduleAth, name='Training-Schedule'),
+    path('athlete-dashboard/', views.athDash, name='Training-AthleteDashboard'),
+    path('coach-dashboard/', views.coachDash, name='Training-CoachDashboard'),
+    
+    path('coach-dashboard/addThrowExercise/', views.editThrowdesc, name='Training-AddThrow'),
+    path('coach-dashboard/addThrowSchedule/', views.editThrowschedule, name='Training-AddThrowSchedule'),
+    path('coach-dashboard/addThrowPlan/', views.editThrowplan, name='Training-AddThrowPlan'),
+    path('coach-dashboard/addLiftExercise/', views.editLiftdesc , name='Training-AddLift'),
+    path('coach-dashboard/addLiftSchedule', views.editLiftschedule , name='Training-AddLiftSchedule'),
+    path('coach-dashboard/addLiftPlan', views.editLiftplan , name='Training-AddLiftPlan'),
+
+    #Make URL/View/Template for:
+    # Lifting Exercises,Plans,Schedule
+
+    #Lifting Data Entry form and Template
+
+    # How am I going to visualize data.
+    ]
+
+
+## Login/Logout Django urls
+urlpatterns += [
+        path('accounts/', include('django.contrib.auth.urls')),
+    ]
